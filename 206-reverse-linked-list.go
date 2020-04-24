57bf6d69eee6a126a9047c89cd71d71f4d4b4990
luofeng <luofengsummer@gmail.com>
2020-04-24 17:53:00 +0800

    feat: 🎸 206. 反转链表

---
+++ 206-reverse-linked-list.go
@@ -0,0 +1,57 @@
+package leetcode
+
+import "github.com/luofeng1/leetcode/link"
+
+/**
+反转一个单链表。
+
+示例:
+
+输入: 1->2->3->4->5->NULL
+输出: 5->4->3->2->1->NULL
+进阶:
+你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/reverse-linked-list
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+/**
+2 为例:
+因为: 2.Next = 1
+cur = 2
+pre = 1
+cur.Next = 1 : pre
+pre = 2 cur
+cur = 3 cur.Next 需提前保存
+如果 cur = nil 则停止;
+
+链表思路:从中间获取一个;看如何去操作
+*/
+func reverseList(head *link.ListNode) *link.ListNode {
+	cur := head
+	var prev *link.ListNode
+	var middle *link.ListNode
+	for cur != nil {
+		middle = cur.Next
+		cur.Next = prev
+		prev = cur
+		cur = middle
+	}
+	return prev
+}
+
+/**
+输入: [1,2,3,4,5]
+输出: [5,4,3,2,1]
+预期结果: [5,4,3,2,1]
+*/
